ad2c3c9df2fccb77081d4a7c20dd6c082cfbf5a8
AntaresScoro <antares.scoro@gmail.com>
2025-11-28 14:51:33 +0100

    Integrate Mongoose and configure MongoDB connection in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ClipsModule } from './clips/clips.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ClipsModule],
+  imports: [
+    ClipsModule,
+    MongooseModule.forRoot(
+      'mongodb://root:example@localhost:27018/nest-clips?authSource=admin',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
