1b55218b68f3dbb8665c2212f2470dcb73073a9c
AntaresScoro <antares.scoro@gmail.com>
2025-12-03 15:16:31 +0100

    Integrate global `HttpExceptionFilter` to standardize error handling in the application bootstrap.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,7 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+  app.useGlobalFilters(new HttpExceptionFilter());
   const port = process.env.PORT ?? 3000;
   await app.listen(port);
   console.log(`Server running on port http://localhost:${port}`);
