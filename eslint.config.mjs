016eb1e5b2d8681fd2dc09b6deb5bd2f620b50cb
AntaresScoro <antares.scoro@gmail.com>
2025-12-16 11:16:12 +0100

    Update ESLint config: add `__dirname` resolution using `fileURLToPath` for node environment compatibility

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,11 @@ import eslint from '@eslint/js';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default tseslint.config(
   {
